84b48162c7d286e2939dc3f4830da011fda7e987
Yaroslav Kulakov <ykulakov00@gmail.com>
2025-12-20 00:01:29 +0200

    refactor: skip login test on CI environment

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -6,6 +6,8 @@ import { AccountPage } from '../Pages/Account.page';
 
 // eslint-disable-next-line playwright/expect-expect
 test('Verify login with valid credentials', async ({ page }) => {
+  
+  test.skip(process.env.CI === 'true', 'Skip test on CI');
   const homePage = new HomePage(page);
   const loginPage = new LoginPage(page);
   const accountPage = new AccountPage(page);
